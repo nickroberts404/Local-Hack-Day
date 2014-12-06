083e02e3a770a63784e78be2f04dd5cb2aaf6d20
Michael Volk <michaelvolk@utexas.edu>
2014-12-06 12:29:11 -0600

    Upcoming Movies

---
+++ js/upcomingMovies.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+        $.ajax({
+            type: 'GET',
+            url: 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/upcoming.json?page_limit=16&page=1&country=us&apikey=mhev5k6bwufz7q5drc9yk7nr',
+            dataType: 'jsonp',
+            success: function (data) {
+                console.log(data);
+            }
+        });
+});
