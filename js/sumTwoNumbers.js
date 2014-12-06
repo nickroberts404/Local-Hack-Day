82190405f675c8f83001501fc3fb54b25ed39a9f
Michael Volk <michaelvolk@utexas.edu>
2014-12-06 12:23:46 -0600

    In Theaters

--- js/sumTwoNumbers.js
+++ js/sumTwoNumbers.js
@@ -1,7 +1,11 @@
 
-console.log(sumTwoNumbers(4, 6));
-
-
-function sumTwoNumbers(a, b) {
-	return a + b;
-}
+$(document).ready(function () {
+        $.ajax({
+            type: 'GET',
+            url: 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?page_limit=16&page=1&country=us&apikey=mhev5k6bwufz7q5drc9yk7nr',
+            dataType: 'jsonp',
+            success: function (data) {
+                console.log(data);
+            }
+        });
+});
